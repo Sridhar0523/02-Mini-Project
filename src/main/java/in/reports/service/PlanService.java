e2ac155a3f7c8de5b7d086bb3fa566bc1101f9d6
Sridhar0523 <log4sridhar@gmail.com>
2022-12-21 08:30:16 +0530

    updated

--- src/main/java/in/reports/service/PlanService.java
+++ src/main/java/in/reports/service/PlanService.java
@@ -1,5 +1,13 @@
 package in.reports.service;
 
+import java.util.List;
+
+import in.reports.bindings.Entries;
+import in.reports.bindings.Plan;
+
 public interface PlanService {
 
+	public List<Plan> getplanName(String planName);
+
+	public Plan getplanStaus(String planStatus);
 }
--- src/main/java/in/reports/service/PlanServiceImpl.java
+++ src/main/java/in/reports/service/PlanServiceImpl.java
@@ -1,5 +1,29 @@
 package in.reports.service;
 
-public class PlanServiceImpl {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import in.reports.bindings.Plan;
+
+@Service
+public class PlanServiceImpl implements PlanService {
+	
+	@Autowired
+	private PlanService planService;
+
+	@Override
+	public List<Plan> getplanName(String planName) {
+		List<Plan> getplanName = planService.getplanName(planName);
+		return getplanName;
+	}
+
+	@Override
+	public Plan getplanStaus(String planStatus) {
+		Plan getplanStaus = planService.getplanStaus(planStatus);
+		return getplanStaus;
+	}
+
+	
 }
